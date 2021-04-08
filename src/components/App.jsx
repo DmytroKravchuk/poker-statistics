33592453cea195c698752b2dee65aad249829c23
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2021-04-09 01:01:04 +0300

    updated logo page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Provider } from "react-redux";
-import configureStore from "@redux/store";
-import LoginPage from "@components/LoginPage";
+import { Provider } from 'react-redux';
+import configureStore from '@redux/store';
+import LoginPage from '@components/LoginPage';
 
 const store = configureStore();
 
-const App = () => {
-    return (
-        <Provider store={ store }>
-            <div className="App">
-                <LoginPage />
-            </div>
-        </Provider>
-    )
-}
+const App = () => (
+  <Provider store={store}>
+    <div className="App">
+      <LoginPage />
+    </div>
+  </Provider>
+);
 
 export default App;
--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,81 +1,89 @@
-import React from "react";
+import React from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
-import { useSelector } from "react-redux";
-import localeReducer from "@redux/reducers/locale";
+import { useSelector } from 'react-redux';
+import './index.scss';
+import Logo from '../../assets/img/logo.jpg';
 
 const layout = {
-    labelCol: {
-        span: 8,
-    },
-    wrapperCol: {
-        span: 16,
-    },
+  layout: 'vertical',
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 24,
+  },
 };
 const tailLayout = {
-    wrapperCol: {
-        offset: 8,
-        span: 16,
-    },
+  wrapperCol: {
+    offset: 0,
+    span: 24,
+  },
 };
 
 const LoginPage = () => {
-    const locale = useSelector( state => state.localeReducer.locale.login_page );
+  const locale = useSelector((state) => state.localeReducer.locale.login_page);
 
-    const onFinish = (values) => {
-        console.log( 'Success:', values );
-    };
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log( 'Failed:', errorInfo );
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
-    return (
-        <Form
-            { ...layout }
-            name="basic"
-            initialValues={ {
-                remember: true,
-            } }
-            onFinish={ onFinish }
-            onFinishFailed={ onFinishFailed }
+  return (
+    <div className="login-page">
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <div className="logo">
+          <img
+            src={Logo}
+            alt="logo"
+            className="logo-img"
+          />
+        </div>
+        <Form.Item
+          label={locale.username_field}
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: locale.required_messages.login,
+            },
+          ]}
         >
-            <Form.Item
-                label={locale.username_field}
-                name="username"
-                rules={ [
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ] }
-            >
-                <Input/>
-            </Form.Item>
-
-            <Form.Item
-                label={locale.password_field}
-                name="password"
-                rules={ [
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ] }
-            >
-                <Input.Password/>
-            </Form.Item>
-
-            <Form.Item { ...tailLayout } name="remember" valuePropName="checked">
-                <Checkbox>{ locale.remember_me_field }</Checkbox>
-            </Form.Item>
-
-            <Form.Item { ...tailLayout }>
-                <Button type="primary" htmlType="submit">
-                    { locale.submit }
-                </Button>
-            </Form.Item>
-        </Form>
-    )
-}
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label={locale.password_field}
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: locale.required_messages.password,
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>{locale.remember_me_field}</Checkbox>
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            {locale.submit}
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginPage;
